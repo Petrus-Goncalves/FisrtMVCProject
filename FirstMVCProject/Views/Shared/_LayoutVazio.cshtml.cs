9a65808b139e09ffc6bcdd1419973cf1c5c9fd11
Pétrus Gonçalves Teixeira <petrusgoncalves@outlook.com>
2026-01-16 18:11:46 -0300

    Criação do layout vazio de login

---
+++ FirstMVCProject/Views/Shared/_LayoutVazio.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstMVCProject.Views.Shared
+{
+    public class _LayoutVazioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
