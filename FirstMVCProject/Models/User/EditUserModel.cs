ee7876896f18e048954319f9ba6e206f3f2c6a1c
Pétrus Gonçalves Teixeira <petrusgoncalves@outlook.com>
2026-01-15 22:22:38 -0300

    Criação de nova pasta User para juntar models de usuário

---
+++ FirstMVCProject/Models/User/EditUserModel.cs
@@ -0,0 +1,33 @@
+﻿using FirstMVCProject.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FirstMVCProject.Models.User
+{
+    public class EditUserModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do usuário é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O login do usuário é obrigatório")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O email do usuário é obrigatório")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Password)]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
+        public string ConfirmarSenha { get; set; }
+
+        [Required(ErrorMessage = "O perfil do usuário é obrigatório")]
+        public EnumPerfilUsuario? Perfil { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+    }
+}
---
+++ FirstMVCProject/Models/User/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using FirstMVCProject.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FirstMVCProject.Models.User
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O nome do contato é obrigatório")]
+        public string Nome { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+        [Required]
+        [NotMapped]
+        [DataType(DataType.Password)]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
+        public string ConfirmarSenha { get; set; }
+        public string Email { get; set; }
+        public EnumPerfilUsuario? Perfil { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+    }
+}
