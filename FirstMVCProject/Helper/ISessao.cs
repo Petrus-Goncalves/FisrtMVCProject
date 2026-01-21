06e9cfa1bd708e1ca63e9080849271f29dc8b4aa
Pétrus Gonçalves Teixeira <petrusgoncalves@outlook.com>
2026-01-21 20:54:41 -0300

    Criação da classe de sessão do usuário e a interface correspondente. Criação da página cshtml de logout

---
+++ FirstMVCProject/Helper/ISessao.cs
@@ -0,0 +1,11 @@
+﻿using FirstMVCProject.Models.User;
+
+namespace FirstMVCProject.Helper
+{
+    public interface ISessao
+    {
+        void CriarSessaoUsuario(UserModel user);
+        void RemoverSessaoUsuario();
+        UserModel BuscarSessaoUsuario();
+    }
+}
---
+++ FirstMVCProject/Helper/Sessao.cs
@@ -0,0 +1,39 @@
+﻿using FirstMVCProject.Models.User;
+using Newtonsoft.Json;
+
+namespace FirstMVCProject.Helper
+{
+    public class Sessao : ISessao
+    {
+        private readonly IHttpContextAccessor _httpContext;
+
+        public Sessao(IHttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext; 
+        }
+
+        public UserModel BuscarSessaoUsuario()
+        {
+            string sessaoUsuario = _httpContext.HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoUsuario))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<UserModel>(sessaoUsuario);
+        }
+
+        public void CriarSessaoUsuario(UserModel user)
+        {
+            string usuario = JsonConvert.SerializeObject(user);
+
+            _httpContext.HttpContext.Session.SetString("sessaoUsuarioLogado", usuario);
+        }
+
+        public void RemoverSessaoUsuario()
+        {
+            _httpContext.HttpContext.Session.Remove("sessaoUsuarioLogado");
+        }
+    }
+}
